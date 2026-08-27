225fe1ca1263aac0b6e130a8f91aa5c80037ecc2
Raj Singh <rajsingh98447859@gmail.com>
2026-08-27 13:59:38 +0530

    Time: 3 ms (97.37%) | Memory: 19.4 MB (71.05%) - LeetSync

--- 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.py
+++ 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.py
@@ -1,34 +1,29 @@
 class Solution:
     def lexGreaterPermutation(self, s: str, target: str) -> str:
         n = len(s)
-        rem = [0] * 26
-        for ch in s:
-            rem[ord(ch) - 97] += 1
+        sb = s.encode('ascii')
+        tb = target.encode('ascii')
+        cnt = [0] * 26
+        for b in sb:
+            cnt[b - 97] += 1
         matched = 0
         while matched < n:
-            c = ord(target[matched]) - 97
-            if rem[c] > 0:
-                rem[c] -= 1
+            c = tb[matched] - 97
+            if cnt[c] > 0:
+                cnt[c] -= 1
                 matched += 1
             else:
                 break
         if matched == n:
             matched = n - 1
-            rem[ord(target[n - 1]) - 97] += 1
+            cnt[tb[n - 1] - 97] += 1
         for i in range(matched, -1, -1):
-            target_code = ord(target[i]) - 97
-            chosen = -1
-            for c in range(target_code + 1, 26):
-                if rem[c] > 0:
-                    chosen = c
-                    break
-            if chosen != -1:
-                rem[chosen] -= 1
-                suffix = []
-                for c in range(26):
-                    if rem[c] > 0:
-                        suffix.append(chr(c + 97) * rem[c])
-                return target[:i] + chr(chosen + 97) + "".join(suffix)
+            target_c = tb[i] - 97
+            for c in range(target_c + 1, 26):
+                if cnt[c] > 0:
+                    cnt[c] -= 1
+                    suffix = "".join(chr(k + 97) * cnt[k] for k in range(26) if cnt[k])
+                    return target[:i] + chr(c + 97) + suffix
             if i > 0:
-                rem[ord(target[i - 1]) - 97] += 1
+                cnt[tb[i - 1] - 97] += 1
         return ""
