411097093fa5a67c6f54d1e21d6ba220d0524e6f
Raj Singh <rajsingh98447859@gmail.com>
2026-08-29 11:30:19 +0530

    Time: 345 ms (35.07%) | Memory: 46.6 MB (82.09%) - LeetSync

---
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lexicographicallySmallestArray(self, nums: list[int], limit: int) -> list[int]:
+        n = len(nums)
+        sorted_pairs = sorted((val, idx) for idx, val in enumerate(nums))
+        ans = [0] * n
+        i = 0
+        while i < n:
+            j = i
+            while j + 1 < n and sorted_pairs[j + 1][0] - sorted_pairs[j][0] <= limit:
+                j += 1
+            indices = sorted(sorted_pairs[k][1] for k in range(i, j + 1))
+            for k in range(len(indices)):
+                ans[indices[k]] = sorted_pairs[i + k][0]
+            i = j + 1
+        return ans
