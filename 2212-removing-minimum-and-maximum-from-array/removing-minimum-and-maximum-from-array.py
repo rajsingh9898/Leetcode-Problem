687fbe8abd4e7adefb7f71e0486abbe0ad5fddac
Raj Singh <rajsingh98447859@gmail.com>
2026-08-30 21:45:29 +0530

    Time: 13 ms (86.40%) | Memory: 34.1 MB (5.44%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.py
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.py
@@ -1,9 +1,8 @@
-import json, sys
-def calc(a):
-    n, i, j = len(a), *sorted((a.index(min(a)), a.index(max(a))))
-    return str(min(j + 1, n - i, i + 1 + n - j))
-with open("user.out", "w") as f:
-    f.write("\n".join(calc(json.loads(line)) for line in sys.stdin if line.strip()) + "\n")
-exit(0)
 class Solution:
-    minimumDeletions = None
+    def minimumDeletions(self, nums: list[int]) -> int:
+        n = len(nums)
+        i = nums.index(min(nums))
+        j = nums.index(max(nums))
+        if i > j:
+            i, j = j, i
+        return min(j + 1, n - i, i + 1 + n - j)
