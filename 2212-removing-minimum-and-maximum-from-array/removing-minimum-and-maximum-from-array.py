0a054c2c7e53fe7fc704bcd22d6432a355e16dd6
Raj Singh <rajsingh98447859@gmail.com>
2026-08-30 21:47:39 +0530

    Time: 4 ms (99.09%) | Memory: 33.7 MB (24.47%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.py
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.py
@@ -1,8 +1,14 @@
 class Solution:
     def minimumDeletions(self, nums: list[int]) -> int:
         n = len(nums)
+        if n <= 2:
+            return n
         i = nums.index(min(nums))
         j = nums.index(max(nums))
         if i > j:
             i, j = j, i
-        return min(j + 1, n - i, i + 1 + n - j)
+        left = j + 1
+        right = n - i
+        both = i + 1 + n - j
+        ans = left if left < right else right
+        return ans if ans < both else both
