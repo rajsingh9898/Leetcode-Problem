4f1c5d0066ea0305f3edacea144701d0c9a89349
Raj Singh <rajsingh98447859@gmail.com>
2026-08-31 13:29:33 +0530

    Time: 51 ms (99.50%) | Memory: 63.1 MB (57.18%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -1,17 +1,21 @@
-from typing import Optional
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> list[int]:
-        if not head or not head.next or not head.next.next:
-            return [-1, -1]
-        prev = head
+        a = head.val
         curr = head.next
-        idx = 1
+        b = curr.val
+        curr = curr.next
         first_cp = -1
         last_cp = -1
         min_dist = float('inf')
-        while curr.next:
-            if (curr.val > prev.val and curr.val > curr.next.val) or \
-               (curr.val < prev.val and curr.val < curr.next.val):
+        idx = 1
+        while curr:
+            c = curr.val
+            if (b > a and b > c) or (b < a and b < c):
                 if first_cp == -1:
                     first_cp = idx
                 else:
@@ -19,9 +23,10 @@ class Solution:
                     if dist < min_dist:
                         min_dist = dist
                 last_cp = idx
-            prev = curr
+            a = b
+            b = c
             curr = curr.next
             idx += 1
-        if first_cp == -1 or first_cp == last_cp:
+        if first_cp == last_cp:
             return [-1, -1]
         return [min_dist, last_cp - first_cp]
