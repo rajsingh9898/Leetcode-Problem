3c514631bf141c03de74e1c6c18a490d9d976761
Raj Singh <rajsingh98447859@gmail.com>
2026-09-10 21:38:27 +0530

    Time: 50 ms (67.78%) | Memory: 19.6 MB (33.89%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.py
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.py
@@ -1,45 +1,24 @@
+import gc
+gc.disable()
 class Solution:
     def averageOfSubtree(self, root: TreeNode | None) -> int:
-        if not root:
-            return 0
-        stack = [root]
-        order = []
-        stack_pop = stack.pop
-        stack_append = stack.append
-        order_append = order.append
-        while stack:
-            curr = stack_pop()
-            order_append(curr)
-            if curr.left:
-                stack_append(curr.left)
-            if curr.right:
-                stack_append(curr.right)
         ans = 0
-        sums = []
-        counts = []
-        sums_pop = sums.pop
-        counts_pop = counts.pop
-        sums_append = sums.append
-        counts_append = counts.append
-        for node in reversed(order):
-            left = node.left
-            right = node.right
+        def dfs(node):
+            nonlocal ans
             val = node.val
-            if not left and not right:
+            s = val
+            c = 1
+            if node.left:
+                ls, lc = dfs(node.left)
+                s += ls
+                c += lc
+            if node.right:
+                rs, rc = dfs(node.right)
+                s += rs
+                c += rc
+            if s // c == val:
                 ans += 1
-                sums_append(val)
-                counts_append(1)
-            else:
-                s = val
-                c = 1
-                if right:
-                    s += sums_pop()
-                    c += counts_pop()
-                if left:
-                    s += sums_pop()
-                    c += counts_pop()
-                if s // c == val:
-                    ans += 1
-                sums_append(s)
-                counts_append(c)
+            return s, c
+        if root:
+            dfs(root)
         return ans
