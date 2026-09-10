d65d9474af659e9cea1257656cae3d01d9e6991e
Raj Singh <rajsingh98447859@gmail.com>
2026-09-10 21:37:05 +0530

    Time: 58 ms (19.37%) | Memory: 19.4 MB (92.37%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.py
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.py
@@ -4,27 +4,42 @@ class Solution:
             return 0
         stack = [root]
         order = []
+        stack_pop = stack.pop
+        stack_append = stack.append
+        order_append = order.append
         while stack:
-            node = stack.pop()
-            order.append(node)
-            if node.left:
-                stack.append(node.left)
-            if node.right:
-                stack.append(node.right)
+            curr = stack_pop()
+            order_append(curr)
+            if curr.left:
+                stack_append(curr.left)
+            if curr.right:
+                stack_append(curr.right)
         ans = 0
+        sums = []
+        counts = []
+        sums_pop = sums.pop
+        counts_pop = counts.pop
+        sums_append = sums.append
+        counts_append = counts.append
         for node in reversed(order):
-            s = node.val
-            c = 1
             left = node.left
-            if left:
-                s += left.s
-                c += left.c
             right = node.right
-            if right:
-                s += right.s
-                c += right.c
-            if node.val == s // c:
+            val = node.val
+            if not left and not right:
                 ans += 1
-            node.s = s
-            node.c = c
+                sums_append(val)
+                counts_append(1)
+            else:
+                s = val
+                c = 1
+                if right:
+                    s += sums_pop()
+                    c += counts_pop()
+                if left:
+                    s += sums_pop()
+                    c += counts_pop()
+                if s // c == val:
+                    ans += 1
+                sums_append(s)
+                counts_append(c)
         return ans
