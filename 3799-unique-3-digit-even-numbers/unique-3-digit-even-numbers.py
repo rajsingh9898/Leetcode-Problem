97947bd4f45e97a7010b3502dbdbfa1b1973112e
Raj Singh <rajsingh98447859@gmail.com>
2026-09-11 11:19:44 +0530

    Time: 3 ms (99.78%) | Memory: 19.2 MB (72.53%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
@@ -1,9 +1,25 @@
-from itertools import permutations
 class Solution:
     def totalNumbers(self, digits: list[int]) -> int:
-        return len({
-            a * 100 + b * 10 + c
-            for a, b, c in permutations(digits, 3)
-            if a != 0 and not (c & 1)
-        })
+        cnt = [0] * 10
+        for d in digits:
+            cnt[d] += 1
+        evens = [d for d in (0, 2, 4, 6, 8) if cnt[d]]
+        if not evens:
+            return 0
+        avail = [d for d in range(10) if cnt[d]]
+        ans = 0
+        for d1 in avail:
+            if d1 == 0:
+                continue
+            cnt[d1] -= 1
+            for d2 in avail:
+                if not cnt[d2]:
+                    continue
+                cnt[d2] -= 1
+                for d3 in evens:
+                    if cnt[d3]:
+                        ans += 1
+                cnt[d2] += 1
+            cnt[d1] += 1
+        return ans
     unique3DigitEvenNumbers = totalNumbers
