8e7da02107faa24c68b14fc4eac0d84321795ca3
Raj Singh <rajsingh98447859@gmail.com>
2026-09-12 21:12:10 +0530

    Time: 1261 ms (96.61%) | Memory: 72.6 MB (67.80%) - LeetSync

--- 3562-maximum-score-of-non-overlapping-intervals/maximum-score-of-non-overlapping-intervals.py
+++ 3562-maximum-score-of-non-overlapping-intervals/maximum-score-of-non-overlapping-intervals.py
@@ -1,25 +1,39 @@
 from bisect import bisect_left
 class Solution:
     def maximumWeight(self, intervals: list[list[int]]) -> list[int]:
-        n = len(intervals)
         indexed = sorted(
-            ((intervals[i][0], intervals[i][1], intervals[i][2], i) for i in range(n)),
+            ((iv[0], iv[1], iv[2], i) for i, iv in enumerate(intervals)),
             key=lambda x: (x[1], x[0], x[3])
         )
+        n = len(indexed)
         end_times = [iv[1] for iv in indexed]
-        INF_STATE = (1, ())
-        dp = [[INF_STATE] * n for _ in range(5)]
-        for i in range(n):
-            l, r, w, orig_id = indexed[i]
-            p = bisect_left(end_times, l) - 1
+        dp = [[(0, ())] * (n + 1) for _ in range(5)]
+        for i, (l, r, w, orig_id) in enumerate(indexed):
+            p = bisect_left(end_times, l)
             for c in range(1, 5):
-                cand = INF_STATE
-                if c == 1:
-                    cand = (-w, (orig_id,))
-                elif p >= 0 and dp[c - 1][p] != INF_STATE:
-                    prev_w_neg, prev_idx = dp[c - 1][p]
-                    cand = (prev_w_neg - w, tuple(sorted(prev_idx + (orig_id,))))
-                opt1 = dp[c][i - 1] if i > 0 else INF_STATE
-                dp[c][i] = min(cand, opt1)
-        best = min(dp[c][n - 1] for c in range(1, 5))
-        return list(best[1])
+                opt_w, opt_idx = dp[c][i]
+                prev_w, prev_idx = dp[c - 1][p]
+                if c > 1 and prev_w == 0:
+                    dp[c][i + 1] = dp[c][i]
+                    continue
+                cur_w = prev_w + w
+                if cur_w < opt_w:
+                    dp[c][i + 1] = dp[c][i]
+                elif cur_w > opt_w:
+                    dp[c][i + 1] = (cur_w, tuple(sorted(prev_idx + (orig_id,))))
+                else:
+                    cand_idx = tuple(sorted(prev_idx + (orig_id,)))
+                    if cand_idx < opt_idx:
+                        dp[c][i + 1] = (cur_w, cand_idx)
+                    else:
+                        dp[c][i + 1] = dp[c][i]
+        best_w = 0
+        best_idx = ()
+        for c in range(1, 5):
+            w, idx = dp[c][n]
+            if w > best_w:
+                best_w = w
+                best_idx = idx
+            elif w == best_w and w > 0 and idx < best_idx:
+                best_idx = idx
+        return list(best_idx)
