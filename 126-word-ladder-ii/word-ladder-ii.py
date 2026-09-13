c260c0205bcb43e382fae5a21869443566c22d9d
Raj Singh <rajsingh98447859@gmail.com>
2026-09-14 00:21:36 +0530

    Time: 4 ms (99.15%) | Memory: 19.8 MB (50.07%) - LeetSync

--- 126-word-ladder-ii/word-ladder-ii.py
+++ 126-word-ladder-ii/word-ladder-ii.py
@@ -1,36 +1,40 @@
 from collections import defaultdict
 class Solution:
     def findLadders(self, beginWord: str, endWord: str, wordList: list[str]) -> list[list[str]]:
-        words = set(wordList)
-        if endWord not in words:
+        unvisited = set(wordList)
+        if endWord not in unvisited:
             return []
-        layer = {beginWord}
-        words.discard(beginWord)
+        unvisited.discard(beginWord)
+        L = len(beginWord)
+        buckets = defaultdict(list)
+        for w in unvisited:
+            for i in range(L):
+                buckets[w[:i] + '*' + w[i + 1:]].append(w)
         parents = defaultdict(list)
+        layer = {beginWord}
         found = False
         while layer and not found:
-            words -= layer
+            unvisited -= layer
             next_layer = set()
-            for word in layer:
-                for i in range(len(word)):
-                    p1, p2 = word[:i], word[i + 1:]
-                    for c in 'abcdefghijklmnopqrstuvwxyz':
-                        cand = p1 + c + p2
-                        if cand in words:
-                            parents[cand].append(word)
-                            next_layer.add(cand)
-                            if cand == endWord:
+            for w in layer:
+                for i in range(L):
+                    pat = w[:i] + '*' + w[i + 1:]
+                    for nxt in buckets[pat]:
+                        if nxt in unvisited:
+                            parents[nxt].append(w)
+                            next_layer.add(nxt)
+                            if nxt == endWord:
                                 found = True
             layer = next_layer
         if not found:
             return []
         res = []
         path = [endWord]
-        def dfs(node: str) -> None:
-            if node == beginWord:
+        def dfs(u: str) -> None:
+            if u == beginWord:
                 res.append(path[::-1])
                 return
-            for p in parents[node]:
+            for p in parents[u]:
                 path.append(p)
                 dfs(p)
                 path.pop()
