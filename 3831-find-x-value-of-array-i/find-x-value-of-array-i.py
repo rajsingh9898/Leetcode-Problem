72c6a1affc360407d0d67a1793c888ce9eda5262
Raj Singh <rajsingh98447859@gmail.com>
2026-09-21 11:49:49 +0530

    Time: 55 ms (100.00%) | Memory: 34.2 MB (52.63%) - LeetSync

--- 3831-find-x-value-of-array-i/find-x-value-of-array-i.py
+++ 3831-find-x-value-of-array-i/find-x-value-of-array-i.py
@@ -1,15 +1,65 @@
 class Solution:
     def resultArray(self, nums: list[int], k: int) -> list[int]:
-        ans = [0] * k
-        dp = [0] * k
-        for num in nums:
-            v = num % k
-            new_dp = [0] * k
-            new_dp[v] += 1
-            for r in range(k):
-                if dp[r]:
-                    new_dp[(r * v) % k] += dp[r]
-            for r in range(k):
-                ans[r] += new_dp[r]
-            dp = new_dp
-        return ans
+        n = len(nums)
+        total = n * (n + 1) // 2
+        if k == 1:
+            return [total]
+        if k == 2:
+            dp1 = ans1 = 0
+            for x in nums:
+                if x % 2:
+                    dp1 += 1
+                else:
+                    dp1 = 0
+                ans1 += dp1
+            return [total - ans1, ans1]
+        if k == 3:
+            dp1 = dp2 = 0
+            ans1 = ans2 = 0
+            for x in nums:
+                v = x % 3
+                if v == 1:
+                    dp1 += 1
+                elif v == 2:
+                    dp1, dp2 = dp2, dp1 + 1
+                else:
+                    dp1 = dp2 = 0
+                ans1 += dp1
+                ans2 += dp2
+            return [total - ans1 - ans2, ans1, ans2]
+        if k == 4:
+            dp1 = dp2 = dp3 = 0
+            ans1 = ans2 = ans3 = 0
+            for x in nums:
+                v = x % 4
+                if v == 1:
+                    dp1 += 1
+                elif v == 3:
+                    dp1, dp3 = dp3, dp1 + 1
+                elif v == 2:
+                    dp1, dp2, dp3 = 0, dp1 + dp3 + 1, 0
+                else:
+                    dp1 = dp2 = dp3 = 0
+                ans1 += dp1
+                ans2 += dp2
+                ans3 += dp3
+            return [total - ans1 - ans2 - ans3, ans1, ans2, ans3]
+        dp1 = dp2 = dp3 = dp4 = 0
+        ans1 = ans2 = ans3 = ans4 = 0
+        for x in nums:
+            v = x % 5
+            if v == 1:
+                dp1 += 1
+            elif v == 2:
+                dp1, dp2, dp3, dp4 = dp3, dp1 + 1, dp4, dp2
+            elif v == 3:
+                dp1, dp2, dp3, dp4 = dp2, dp4, dp1 + 1, dp3
+            elif v == 4:
+                dp1, dp2, dp3, dp4 = dp4, dp3, dp2, dp1 + 1
+            else:
+                dp1 = dp2 = dp3 = dp4 = 0
+            ans1 += dp1
+            ans2 += dp2
+            ans3 += dp3
+            ans4 += dp4
+        return [total - ans1 - ans2 - ans3 - ans4, ans1, ans2, ans3, ans4]
