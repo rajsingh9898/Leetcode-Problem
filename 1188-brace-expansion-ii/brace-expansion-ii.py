21910f7fcfda5c38778c11d7235fce5bf8b22d87
Raj Singh <rajsingh98447859@gmail.com>
2026-09-25 11:40:51 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (56.83%) - LeetSync

--- 1188-brace-expansion-ii/brace-expansion-ii.py
+++ 1188-brace-expansion-ii/brace-expansion-ii.py
@@ -1,22 +1,40 @@
+from itertools import product
 class Solution:
     def braceExpansionII(self, expression: str) -> list[str]:
         if "{" not in expression:
-            return [expression]
-        stack = [expression]
-        seen = {expression}
-        res = set()
-        while stack:
-            s = stack.pop()
-            if "{" not in s:
-                res.add(s)
-                continue
-            j = s.find("}")
-            i = s.rfind("{", 0, j)
-            head = s[:i]
-            tail = s[j + 1:]
-            for sub in s[i + 1:j].split(","):
-                nxt = head + sub + tail
-                if nxt not in seen:
-                    seen.add(nxt)
-                    stack.append(nxt)
-        return sorted(res)
+            return sorted(set(expression.split(",")))
+        groups = [[]]
+        level = 0
+        i = 0
+        n = len(expression)
+        while i < n:
+            c = expression[i]
+            if c == '{':
+                if level == 0:
+                    start = i + 1
+                level += 1
+                i += 1
+            elif c == '}':
+                level -= 1
+                if level == 0:
+                    groups[-1].append(self.braceExpansionII(expression[start:i]))
+                i += 1
+            elif c == ',' and level == 0:
+                groups.append([])
+                i += 1
+            elif level == 0:
+                j = i
+                while j < n and expression[j].isalpha():
+                    j += 1
+                groups[-1].append([expression[i:j]])
+                i = j
+            else:
+                i += 1
+        ans = set()
+        for group in groups:
+            if len(group) == 1:
+                ans.update(group[0])
+            elif group:
+                for prod in product(*group):
+                    ans.add("".join(prod))
+        return sorted(ans)
