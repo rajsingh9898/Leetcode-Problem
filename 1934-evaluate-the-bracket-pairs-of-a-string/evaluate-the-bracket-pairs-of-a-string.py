8b766712d0a2a28863394f5ee5b2cf9de61b4d76
Raj Singh <rajsingh98447859@gmail.com>
2026-09-26 17:44:53 +0530

    Time: 27 ms (99.38%) | Memory: 51.9 MB (28.97%) - LeetSync

--- 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.py
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.py
@@ -1,6 +1,9 @@
 class Solution:
     def evaluate(self, s: str, knowledge: list[list[str]]) -> str:
+        if '(' not in s:
+            return s
         d = dict(knowledge)
+        get = d.get 
         parts = s.replace(')', '(').split('(')
-        parts[1::2] = [d.get(k, '?') for k in parts[1::2]]
+        parts[1::2] = [get(k, '?') for k in parts[1::2]]
         return "".join(parts)
